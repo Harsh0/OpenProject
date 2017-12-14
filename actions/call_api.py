e9f1d39ad38728bd649ed8e5e426ca55f6ac25dd
Harsh Singhal <harsh.singhal040@gmail.com>
2017-12-14 11:32:40 +0530

    action modified

--- actions/call_api.py
+++ actions/call_api.py
@@ -8,5 +8,6 @@ __all__ = [
 
 class OutputAction(Action):
     def run(self, url,body):
-        print('payload',url, body)
+        response = requests.get(url+"?body="+body)
+        print(response)
         return True
